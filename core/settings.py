3dcd093587ad7ea80a8d0cb1a0e776351c604933
Unknown-zzz <unknown.zzz.2909@gmail.com>
2026-05-05 11:43:28 -0400

    feat: initialize Django core settings with database, authentication, and logging configurations

--- core/settings.py
+++ core/settings.py
@@ -75,7 +75,7 @@ if DB_ENGINE == 'django.db.backends.sqlite3':
     }
 else:
     import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=600)
+    db_from_env = dj_database_url.config(conn_max_age=None)
     if db_from_env:
         DATABASES = {'default': db_from_env}
     else:
